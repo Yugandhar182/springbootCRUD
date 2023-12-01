ebb21e738ed38ffeae92aebe35abe48328f3868f
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-12-01 15:28:37 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,26 @@
-package com.example.demo.controller;
+package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-@Controller
-public class HelloController {
+@SpringBootApplication
+public class DemoApplication {
 
-	@GetMapping("/")
-	public String hello() {
-		
-		return "hello";
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Controller
+	public class HelloController {
+
+		@GetMapping("/")
+		public String hello() {
+			
+			return "form";
+		}
 	}
 }
